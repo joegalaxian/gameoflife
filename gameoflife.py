587a4dc3609a5aca9069c3fc512c876ef2da1df4
joegalaxian <joegalaxian@users.noreply.github.com>
2018-03-15 11:09:26 +1100

    Minor improvements
    - Created: self.run()
    - Created: self.refresh_population()
    - Improved: clear screen based on platform, and now part of self.render()
    - Improved: FPS now is parameter of Game()
    - Renamed: ALIVE to LIVING

--- gameoflife.py
+++ gameoflife.py
@@ -3,44 +3,58 @@
 import random
 import time
 import os
+import sys
 
-DEAD  = '.'
-ALIVE = '@'
-FPS   = 5 # frames per second
+DEAD   = '.'
+LIVING = '@'
 
 class Game(object):
-	def __init__(self, x=10, y=10, percentage=40):
-		self.gen = 0
-		self.x = x
-		self.y = y
-		self.population = int(x*y*percentage/100)
-
+	def __init__(self, x=30, y=30, population_percentage=30, fps=2):
+		self.generation = 0
+		self.population = 0
+		self.x = x     # board's x axis
+		self.y = y     # board's y axis
+		self.fps = fps # frames per second
 		# create board
 		self.board = self.empty_board(self.x, self.y)
-
 		# populate board
-		for i in range(self.population):
+		for i in range(int(self.x*self.y*population_percentage/100)):
 			while True:
 				rx = random.randint(0, self.x -1)
 				ry = random.randint(0, self.y -1)
 				if self.board[ry][rx] == DEAD:
-					self.board[ry][rx] = ALIVE
+					self.board[ry][rx] = LIVING
 					break
+		# refresh population
+		self.refresh_population()
+
+	def run(self):
+		while self.population:
+			self.render()
+			self.tick()
+			time.sleep(1.0/self.fps)
 
 	def empty_board(self, x, y):
 		return [ [DEAD for ix in range(x)] for iy in range(y) ]
 
 	def render(self):
-		print("Game Of Life - @joegalaxian")
-		print("Board size: %dx%d - Generation: %d - Population: %d" % (self.x, self.y, self.gen, self.population))
+		# Clear screen
+		if sys.platform in ('linux2', 'linux', 'unix', 'posix', 'darwin'):
+			os.system('clear')
+		elif sys.platform in ('win32', 'win64', 'cywin'):
+			os.system('cls')
+		# Print title and stats
+		print("Conway's Game Of Life - @joegalaxian")
+		print("Board size: %dx%d - Generation: %d - Population: %d" % (self.x, self.y, self.generation, self.population))
+		# Print board
 		result = ''
 		for line in self.board:
 			for cell in line:
 				result += str(cell) + ' '
 			result += '\n'
 		result = result[:-1]
-		print result
-
+		print(result)
+			
 	def tick(self):
 		"""
 		Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.
@@ -50,78 +64,61 @@ class Game(object):
 		3. Any live cell with more than three live neighbours dies, as if by overpopulation.
 		4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 		"""
-		new_board = self.empty_board(self.x, self.y)
+		# calculate next generation
+		next_generation_board = self.empty_board(self.x, self.y)
 		for iy in range(self.y):
 			for ix in range(self.x):
 				cell = self.board[iy][ix]
 				neig = self.living_neighbours(ix, iy)
 				new_cell = DEAD
-				if cell == ALIVE and neig < 2: new_cell = DEAD
-				elif cell == ALIVE and neig in(2, 3): new_cell = ALIVE
-				elif cell == ALIVE and neig > 3: new_cell = DEAD
-				elif cell == DEAD and neig == 3: new_cell = ALIVE
-				new_board[iy][ix] = new_cell
-
-		# flip the board
-		self.gen += 1
-		self.board = new_board[:]
-
-		# recalculate population
+				if cell == LIVING and neig < 2: new_cell = DEAD
+				elif cell == LIVING and neig in(2, 3): new_cell = LIVING
+				elif cell == LIVING and neig > 3: new_cell = DEAD
+				elif cell == DEAD and neig == 3: new_cell = LIVING
+				next_generation_board[iy][ix] = new_cell
+		# transit to next generation
+		self.board = next_generation_board[:] # flip board
+		self.generation += 1                  # increase generation
+		self.refresh_population()             # refresh population
+		
+	def refresh_population(self):
 		self.population = 0
 		for line in self.board:
-			self.population += line.count(ALIVE)
+			self.population += line.count(LIVING)
 
 	def living_neighbours(self, x, y):
 		r = 0
-
-		# previous line
+		# count living neighbours from previous line
 		try:
-			if (self.board[y-1][x-1] == ALIVE): r +=1
+			if (self.board[y-1][x-1] == LIVING): r +=1
 		except:
 			pass
-
 		try:
-			if (self.board[y-1][x] == ALIVE): r +=1
+			if (self.board[y-1][x] == LIVING): r +=1
 		except:
 			pass
-
 		try:
-			if (self.board[y-1][x+1] == ALIVE): r +=1
+			if (self.board[y-1][x+1] == LIVING): r +=1
 		except: pass
-
-		# same line
+		# count living neighbours from same line
 		try:
-			if (self.board[y][x-1] == ALIVE): r +=1
+			if (self.board[y][x-1] == LIVING): r +=1
 		except: pass
 		try:
-			if (self.board[y][x+1] == ALIVE): r +=1
+			if (self.board[y][x+1] == LIVING): r +=1
 		except: pass
-
-		# next line
+		# count living neighbours from next line
 		try:
-			if (self.board[y+1][x-1] == ALIVE): r +=1
-		except: pass
-		try:
-			if (self.board[y+1][x] == ALIVE): r +=1
+			if (self.board[y+1][x-1] == LIVING): r +=1
 		except: pass
 		try:
-			if (self.board[y+1][x+1] == ALIVE): r +=1
+			if (self.board[y+1][x] == LIVING): r +=1
 		except: pass
-		
-		#r = random.randint(0,8)
+		try:
+			if (self.board[y+1][x+1] == LIVING): r +=1
+		except: pass
 		return r
 
-
-def main():
-	try:
-		game = Game(50, 30, 25)
-		while game.population:
-			os.system('clear')
-			game.render()
-			game.tick()
-			time.sleep(1.0/FPS)
-	except KeyboardInterrupt:
-		print("Program terminated")
-
 if __name__ == "__main__":
-	main()
+	game = Game()
+	game.run()
